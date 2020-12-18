db4e66067511f0268dc38e424947d27ca306a358
Christopher de Oliveira <christopherforwork@gmail.com>
2020-12-18 15:24:30 -0400

    Finished configs

---
+++ src/Config.php
@@ -0,0 +1,15 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = '/public';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = 'localhost';
+    const DB_DATABASE = 'test';
+    CONST DB_USER = 'dev';
+    const DB_PASS = 'dev';
+
+    const ERROR_CONTROLLER = 'ErrorController';
+    const DEFAULT_ACTION = 'index';
+}
